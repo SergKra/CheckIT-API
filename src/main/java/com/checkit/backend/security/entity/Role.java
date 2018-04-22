6cfb9c3c1a23dccc04f5cc5a514347b4d9369bc8
Gleb Dovzhenko <dovzhenko.gleb@gmail.com>
2018-04-22 14:43:16 +0300

    Add dependency on spring boot jpa  modified:   pom.xml

---
+++ src/main/java/com/checkit/backend/security/entity/Role.java
@@ -0,0 +1,8 @@
+package com.checkit.backend.security.entity;
+
+/**
+ * Created by Gleb Dovzhenko on 21.04.2018.
+ */
+public enum UserRole {
+    
+}
---
+++ src/main/java/com/checkit/backend/security/entity/UserRole.java
@@ -0,0 +1,7 @@
+package com.checkit.backend.security.entity;
+
+/**
+ * Created by Gleb Dovzhenko on 21.04.2018.
+ */
+public class UserRole {
+}
