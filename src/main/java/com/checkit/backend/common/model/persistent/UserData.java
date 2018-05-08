3f9c91a6be25cbdbfcdacc697d9c68f5a14fe5e9
Gleb Dovzhenko <dovzhenko.gleb@gmail.com>
2018-05-08 16:52:41 +0300

    Mocked UserData class for first interaction

--- src/main/java/com/checkit/backend/common/model/persistent/UserData.java
+++ src/main/java/com/checkit/backend/common/model/persistent/UserData.java
@@ -4,14 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
-
 /**
  * Created by Gleb Dovzhenko on 23.04.2018.
  */
-@Entity
+//@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -21,48 +17,48 @@ public class UserData {
         this.registrationEmail = registrationEmail;
     }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+  //  @Id
+  //  @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotEmpty(message="{validation.registrationEmail.NotEmpty.message}")
-    @Size(min=1, max=60, message="{validation.registrationEmail.Size.message}")
-    @Column(name = "email", unique = true)
+  //  @NotEmpty(message="{validation.registrationEmail.NotEmpty.message}")
+  //  @Size(min=1, max=60, message="{validation.registrationEmail.Size.message}")
+  //  @Column(name = "email", unique = true)
     private String registrationEmail;
 
-    @NotEmpty(message="{validation.first_name.NotEmpty.message}")
-    @Size(min=1, max=60, message="{validation.first_name.Size.message}")
-    @Column(name = "first_name")
+  //  @NotEmpty(message="{validation.first_name.NotEmpty.message}")
+  //  @Size(min=1, max=60, message="{validation.first_name.Size.message}")
+  //  @Column(name = "first_name")
     private String firstName;
 
-    @NotEmpty(message="{validation.last_name.NotEmpty.message}")
-    @Size(min=3, max=60, message="{validation.last_name.Size.message}")
-    @Column(name = "last_name")
+  //  @NotEmpty(message="{validation.last_name.NotEmpty.message}")
+  //  @Size(min=3, max=60, message="{validation.last_name.Size.message}")
+  //  @Column(name = "last_name")
     private String lastName;
 
-    @Size(min=3, max=60, message="{validation.phone.Size.message}")
-    @Column(name = "phone")
+  //  @Size(min=3, max=60, message="{validation.phone.Size.message}")
+  //  @Column(name = "phone")
     private String phone;
 
-    @Column(name = "website")
+  //  @Column(name = "website")
     private String websiteURL;
 
-    @NotEmpty(message="{validation.profile_url.NotEmpty.message}")
-    @Size(min=1, max=90, message="{validation.profile_url.Size.message}")
-    @Column(name = "profile_url", unique = true)
+  //  @NotEmpty(message="{validation.profile_url.NotEmpty.message}")
+  //  @Size(min=1, max=90, message="{validation.profile_url.Size.message}")
+  //  @Column(name = "profile_url", unique = true)
     private String profileURL;
 
-    @Column(name = "country")
+  //  @Column(name = "country")
     private String country;
 
-    @Column(name = "city")
+  //  @Column(name = "city")
     private String city;
 
-    @Size(min=1, max=90, message="{validation.profile_photo.Size.message}")
-    @Column(name = "profile_photo")
+ //   @Size(min=1, max=90, message="{validation.profile_photo.Size.message}")
+  //  @Column(name = "profile_photo")
     private String profilePhotoURL;
 
-    @Column(name = "about")
+  //  @Column(name = "about")
     private String about;
 
 }
--- src/main/java/com/checkit/backend/common/reopsitory/UserDataRepository.java
+++ src/main/java/com/checkit/backend/common/reopsitory/UserDataRepository.java
@@ -1,13 +1,8 @@
 package com.checkit.backend.common.reopsitory;
 
-import com.checkit.backend.common.model.persistent.UserData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
 /**
  * Created by Gleb Dovzhenko on 23.04.2018.
  */
-public interface UserDataRepository extends JpaRepository<UserData, Long> {
-    Optional<UserData> findByRegistrationEmail(String username);
+public interface UserDataRepository {// extends JpaRepository<UserData, Long> {
+   // Optional<UserData> findByRegistrationEmail(String username);
 }
